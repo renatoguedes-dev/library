5513b61b0c5206d40eabe4e72598cb103a5f0d3a
Renato Guedes <rmguedes@hotmail.com>
2022-08-05 23:37:36 -0300

    Library's prototype working without inputs

---
+++ script.js
@@ -0,0 +1,112 @@
+let myLibrary = [];
+
+function Book(title, author, pages, language, genre) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.language = language;
+  this.genre = genre;
+  this.read = "not read yet";
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "English", 
+"High Fantasy");
+const harryPotter = new Book("Harry Potter and the Philosopher's Stone", 
+"J. K. Rowling", "223", "English", "Fantasy");
+ 
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotter);
+
+function displayBooks() {
+  let myBooks = document.querySelectorAll('#my-books')[0];
+  
+  for (let i = 0; i < myLibrary.length; i++) {
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    let cardDivContent = `
+      <div id="title-card">
+        <div>
+          <h4>${myLibrary[i].title}</h4>
+        </div>
+        <button class="remove-btn">&times;</button>
+      </div>
+      <p>Author: ${myLibrary[i].author}</p>
+      <p>Number of pages: ${myLibrary[i].pages}</p>
+      <p>Language: English</p>
+      <p>Genre: Fantasy</p>
+      <div class="mark-read">
+        <label for="genre">Have you read this book?</label>
+        <input type="checkbox" name="genre" id="genre" required>
+      </div>
+      <div class="interaction-symbols">
+        <a href="#"><img src="./assets/star-plus-outline.png" alt="add to favorite icon"></a> 
+        <a href="#"><img src="./assets/eye-plus-outline.png" alt="watch later icon"></a>
+        <a href="#"><img src="./assets/share-variant-outline.png" alt="share icon"></a>
+      </div>`
+  
+    cardDiv.innerHTML = cardDivContent;
+    myBooks.append(cardDiv);
+  }
+
+
+}
+
+displayBooks();
+
+// variables for capturing the buttons
+const newBookButton = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const removeBookFromDisplay = document.querySelectorAll('.remove-btn');
+
+
+// buttons being used with their respective events
+
+newBookButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+});
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal)
+  })
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+});
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+removeBookFromDisplay.forEach(button => button.addEventListener('click', removeBooks));
+
+function removeBooks(event) {
+  let removeBook = event.target;
+  removeBook.parentElement.parentElement.remove();
+}
