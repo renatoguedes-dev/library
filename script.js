ac4c8f8dd01cd7b7137a7c644a636202a5d74bcd
Renato Guedes <rmguedes@hotmail.com>
2022-08-25 23:37:17 -0300

    Add variable to store new books added and change minor text

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, pages, language, genre) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.language = language;
-  this.genre = genre;
-  this.read = "not read yet";
+class Book {
+  constructor(title, author, pages, language, genre, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.language = language;
+    this.genre = genre;
+    this.read = read;
+  }
 }
 
 Book.prototype.info = function () {
@@ -14,9 +16,9 @@ Book.prototype.info = function () {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "English", 
-"High Fantasy");
+"High Fantasy", false);
 const harryPotter = new Book("Harry Potter and the Philosopher's Stone", 
-"J. K. Rowling", "223", "English", "Fantasy");
+"J. K. Rowling", "223", "English", "Fantasy", true);
  
 
 function addBookToLibrary(book) {
@@ -32,7 +34,10 @@ function displayBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
     let cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
-    let cardDivContent = `
+    let cardDivContent;
+    
+    if (myLibrary[i].read == true) {
+      cardDivContent = `
       <div id="title-card">
         <div>
           <h4>${myLibrary[i].title}</h4>
@@ -45,26 +50,49 @@ function displayBooks() {
       <p>Genre: Fantasy</p>
       <div class="mark-read">
         <label for="checkbox">Have you read this book?</label>
-        <input type="checkbox" name="checkbox" id="checkbox" required>
+        <input type="checkbox" name="checkbox" id="checkbox" checked>
       </div>
       <div class="interaction-symbols">
         <a href="#"><img src="./assets/star-plus-outline.png" alt="add to favorite icon"></a> 
         <a href="#"><img src="./assets/eye-plus-outline.png" alt="watch later icon"></a>
         <a href="#"><img src="./assets/share-variant-outline.png" alt="share icon"></a>
       </div>`
+
+    } else if (myLibrary[i].read == false) {
+      cardDivContent = `
+        <div id="title-card">
+          <div>
+            <h4>${myLibrary[i].title}</h4>
+          </div>
+          <button class="remove-btn">&times;</button>
+        </div>
+        <p>Author: ${myLibrary[i].author}</p>
+        <p>Number of pages: ${myLibrary[i].pages}</p>
+        <p>Language: English</p>
+        <p>Genre: Fantasy</p>
+        <div class="mark-read">
+          <label for="checkbox">Have you read this book?</label>
+          <input type="checkbox" name="checkbox" id="checkbox">
+        </div>
+        <div class="interaction-symbols">
+          <a href="#"><img src="./assets/star-plus-outline.png" alt="add to favorite icon"></a> 
+          <a href="#"><img src="./assets/eye-plus-outline.png" alt="watch later icon"></a>
+          <a href="#"><img src="./assets/share-variant-outline.png" alt="share icon"></a>
+        </div>`
+    }
   
     cardDiv.innerHTML = cardDivContent;
     myBooks.append(cardDiv);
   }
-
-
 }
 
 displayBooks();
 
+
 // variables for capturing the buttons
 const newBookButton = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const addBookModalBtn = document.querySelector('#add-book-btn')
 const overlay = document.getElementById('overlay');
 const removeBookFromDisplay = document.querySelectorAll('.remove-btn');
 
@@ -110,3 +138,36 @@ function removeBooks(event) {
   let removeBook = event.target;
   removeBook.parentElement.parentElement.remove();
 }
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+};
+
+
+const addNewBook = (e) => {
+  e.preventDefault();
+
+  let myBooks = document.querySelectorAll('#my-books')[0];
+
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let nrOfPages = document.getElementById('pages').value;
+  let language = document.getElementById('language').value;
+  let genre = document.getElementById('genre').value;
+  let readBook = document.getElementById('checkbox').checked;
+  let bookAdded = new Book(title, author, nrOfPages, language, genre, readBook);
+
+  addBookToLibrary(bookAdded);
+ 
+  document.forms[0].reset();
+  closeModal(modal);
+  removeAllChildNodes(myBooks);
+  displayBooks();
+}
+
+addBookModalBtn.addEventListener('click', addNewBook)
+
+
+
